84021b7802a44f81f93191877669211124f0d898
Soll CI <noreply@github.com>
2026-04-12 14:32:39 +0000

    soll 4

---
+++ Formula/soll.rb
@@ -0,0 +1,36 @@
+class Soll < Formula
+  desc "Static import graph analyzer"
+  homepage "https://github.com/marginallyuseful/soll"
+  version "4"
+  license :cannot_represent
+
+  on_macos do
+    if Hardware::CPU.arm?
+      url "https://github.com/marginallyuseful/soll/releases/download/4/soll-darwin-arm64-sha256.txt"
+      sha256 "Not"
+    end
+    if Hardware::CPU.intel?
+      url "undefined"
+      sha256 "undefined"
+    end
+  end
+
+  on_linux do
+    if Hardware::CPU.arm?
+      url "https://github.com/marginallyuseful/soll/releases/download/4/soll-linux-arm64-gnu-sha256.txt"
+      sha256 "Not"
+    end
+    if Hardware::CPU.intel?
+      url "https://github.com/marginallyuseful/soll/releases/download/4/soll-linux-x64-gnu-sha256.txt"
+      sha256 "Not"
+    end
+  end
+
+  def install
+    bin.install "soll"
+  end
+
+  test do
+    system "#{bin}/soll", "--help"
+  end
+end
