ae88610b5acf27dc934456c2c8bff9eaa940a8f3
Soll CI <noreply@github.com>
2026-04-12 14:57:26 +0000

    soll 6

--- Formula/soll.rb
+++ Formula/soll.rb
@@ -1,12 +1,12 @@
 class Soll < Formula
   desc "Static import graph analyzer"
   homepage "https://github.com/marginallyuseful/soll"
-  version "5"
+  version "6"
   license :cannot_represent
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/marginallyuseful/soll/releases/download/5/soll-darwin-arm64-sha256.txt"
+      url "https://github.com/marginallyuseful/soll/releases/download/6/soll-darwin-arm64-sha256.txt"
       sha256 "Not"
     end
     if Hardware::CPU.intel?
@@ -17,11 +17,11 @@ class Soll < Formula
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/marginallyuseful/soll/releases/download/5/soll-linux-arm64-gnu-sha256.txt"
+      url "https://github.com/marginallyuseful/soll/releases/download/6/soll-linux-arm64-gnu-sha256.txt"
       sha256 "Not"
     end
     if Hardware::CPU.intel?
-      url "https://github.com/marginallyuseful/soll/releases/download/5/soll-linux-x64-gnu-sha256.txt"
+      url "https://github.com/marginallyuseful/soll/releases/download/6/soll-linux-x64-gnu-sha256.txt"
       sha256 "Not"
     end
   end
